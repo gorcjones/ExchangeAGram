30b2d9040006f13df80adcbf148fb49cfda21e00
rcjones <rcjones.iphone@gmail.com>
2015-06-02 20:29:41 -0500

    FeedCell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Ryan Jones on 6/2/15.
+//  Copyright (c) 2015 Ryan Jones. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionCell: UILabel!
+}
