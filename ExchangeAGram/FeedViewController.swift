e7ef75825080e4e29813832ad7f5490462ab8f8a
rcjones <rcjones.iphone@gmail.com>
2015-06-03 20:29:59 -0500

    Persisting a FeedItem

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MobileCoreServices
+import CoreData
 
 class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -66,8 +67,20 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let imageData = UIImageJPEGRepresentation(image, 1.0)
+        let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let entityDescription = NSEntityDescription.entityForName("FeedItem", inManagedObjectContext: managedObjectContext!)
+        let feedItem = FeedItem(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+        
+        feedItem.image = imageData
+        feedItem.caption = "Test Caption"
+        
+        (UIApplication.sharedApplication().delegate as AppDelegate).saveContext()
+        
+        
         
         self.dismissViewControllerAnimated(true, completion: nil)
+        
     }
     //UICollectionViewDataSource
     
