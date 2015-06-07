0170cd0b089f36f45704a8e53fe856a79047264c
rcjones <rcjones.iphone@gmail.com>
2015-06-07 05:43:32 -0500

    Setup Collection view from code for FilterViewController

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     var thisFeedItem: FeedItem!
+    
+    var collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 150, height: 150)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +33,17 @@ class FilterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+        
+    
     }
-    */
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+        
+    }
+    
+
 
 }
